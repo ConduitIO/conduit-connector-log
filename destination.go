49ecc080fbcf93f6477c907dd363039a9518ddc7
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-11-04 19:28:10 +0100

    implement connector

--- destination.go
+++ destination.go
@@ -1,14 +1,37 @@
+// Copyright © 2022 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package log
 
 import (
 	"context"
+	"fmt"
+	"strings"
+
 	sdk "github.com/conduitio/conduit-connector-sdk"
+	"github.com/rs/zerolog"
+)
+
+const (
+	ConfigLevel  = "level"
+	LevelDefault = zerolog.InfoLevel
 )
 
 type Destination struct {
 	sdk.UnimplementedDestination
 
-	config DestinationConfig
+	level zerolog.Level
 }
 
 func NewDestination() sdk.Destination {
@@ -17,37 +40,44 @@ func NewDestination() sdk.Destination {
 
 func (d *Destination) Parameters() map[string]sdk.Parameter {
 	return map[string]sdk.Parameter{
-		GlobalConfigParam: {
-			Default:     "localhost:10000",
+		ConfigLevel: {
+			Default:     "INFO",
 			Required:    true,
-			Description: "The URL of the server.",
-		},
-		DestinationConfigParam: {
-			Default:     "false",
-			Required:    false,
-			Description: "An optional configuration parameter.",
+			Description: "The log level used to log records.",
 		},
 	}
 }
 
 func (d *Destination) Configure(ctx context.Context, cfg map[string]string) error {
-	sdk.Logger(ctx).Info().Msg("Configuring a Destination connector...")
-	config, err := ParseDestinationConfig(cfg)
-	if err != nil {
-		return err
+	level := LevelDefault
+	if l := cfg[ConfigLevel]; l != "" {
+		var err error
+		level, err = zerolog.ParseLevel(strings.ToLower(l))
+		if err != nil {
+			return fmt.Errorf(
+				"%q config value %q does not contain a valid level: %w",
+				ConfigLevel, l, err,
+			)
+		}
 	}
-	d.config = config
+	d.level = level
 	return nil
 }
 
 func (d *Destination) Open(ctx context.Context) error {
-	return nil
+	return nil // nothing to open
 }
 
 func (d *Destination) Write(ctx context.Context, records []sdk.Record) (int, error) {
-	return 0, nil
+	logger := sdk.Logger(ctx)
+	for _, r := range records {
+		logger.WithLevel(d.level).
+			RawJSON("record", r.Bytes()).
+			Send()
+	}
+	return len(records), nil
 }
 
 func (d *Destination) Teardown(ctx context.Context) error {
-	return nil
+	return nil // nothing to tear down
 }
--- destination_test.go
+++ destination_test.go
@@ -1,29 +1,112 @@
-package log_test
+// Copyright © 2022 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package log
 
 import (
+	"bufio"
+	"bytes"
 	"context"
-	log "conduitio/conduit-connector-log"
-	"strings"
 	"testing"
+
+	sdk "github.com/conduitio/conduit-connector-sdk"
+	"github.com/matryer/is"
+	"github.com/rs/zerolog"
 )
 
-func TestConfigureDestination_FailsWhenConfigEmpty(t *testing.T) {
-	con := log.Destination{}
-	err := con.Configure(context.Background(), make(map[string]string))
-	if err == nil {
-		t.Error("expected error for missing config params")
+func TestDestination_Configure_Success(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
+
+	testCases := map[string]zerolog.Level{
+		"":      zerolog.InfoLevel,
+		"error": zerolog.ErrorLevel,
+		"warn":  zerolog.WarnLevel,
+		"info":  zerolog.InfoLevel,
+		"debug": zerolog.DebugLevel,
+		"trace": zerolog.TraceLevel,
+		"ERROR": zerolog.ErrorLevel,
+		"WARN":  zerolog.WarnLevel,
+		"INFO":  zerolog.InfoLevel,
+		"DEBUG": zerolog.DebugLevel,
+		"TRACE": zerolog.TraceLevel,
 	}
 
-	if strings.HasPrefix(err.Error(), "config is invalid:") {
-		t.Errorf("expected error to be about missing config, got %v", err)
+	for have, want := range testCases {
+		t.Run(have, func(t *testing.T) {
+			var dest Destination
+			err := dest.Configure(ctx, map[string]string{"level": have})
+			is.NoErr(err)
+			is.Equal(dest.level, want)
+		})
 	}
 }
 
-func TestTeardown_NoOpen(t *testing.T) {
-	con := log.NewDestination()
-	err := con.Teardown(context.Background())
-	if err != nil {
-		t.Errorf("expected no error, got %v", err)
+func TestDestination_Configure_Fail(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
 
+	var dest Destination
+	err := dest.Configure(ctx, map[string]string{"level": "invalid"})
+	is.True(err != nil)
+}
+
+func TestDestination_Configure_Write(t *testing.T) {
+	is := is.New(t)
+
+	var buf bytes.Buffer
+	logger := zerolog.New(&buf)
+	ctx := logger.WithContext(context.Background()) // sdk takes logger from the context
+
+	var dest Destination
+
+	err := dest.Configure(ctx, map[string]string{"level": "warn"})
+	is.NoErr(err)
+
+	have := []sdk.Record{{
+		Position:  []byte("r1"),
+		Operation: sdk.OperationCreate,
+		Metadata:  map[string]string{"foo1": "bar1"},
+		Key:       sdk.RawData("raw-key"),
+		Payload: sdk.Change{
+			Before: nil,
+			After:  sdk.RawData("raw-payload"),
+		},
+	}, {
+		Position:  []byte("r2"),
+		Operation: sdk.OperationUpdate,
+		Metadata:  map[string]string{"foo2": "bar2"},
+		Key:       sdk.StructuredData{"r2-key": 1},
+		Payload: sdk.Change{
+			Before: sdk.StructuredData{"r2-payload": true},
+			After:  sdk.StructuredData{"r2-payload": false},
+		},
+	}}
+
+	want := []string{
+		`{"level":"warn","record":{"position":"cjE=","operation":"create","metadata":{"foo1":"bar1","opencdc.version":"v1"},"key":"cmF3LWtleQ==","payload":{"before":null,"after":"cmF3LXBheWxvYWQ="}}}`,
+		`{"level":"warn","record":{"position":"cjI=","operation":"update","metadata":{"foo2":"bar2","opencdc.version":"v1"},"key":{"r2-key":1},"payload":{"before":{"r2-payload":true},"after":{"r2-payload":false}}}}`,
+	}
+
+	l, err := dest.Write(ctx, have)
+	is.NoErr(err)
+	is.Equal(l, len(have)) // expected destination to write all records
+
+	i := 0
+	scanner := bufio.NewScanner(&buf)
+	for scanner.Scan() {
+		is.Equal(scanner.Text(), want[i])
+		i++
 	}
 }
